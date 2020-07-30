6c557bab6c8b4b05b0c3a76704a25d769b1ac7ea
Paulo Custódio <paulo.custodio@gmail.com>
2020-07-30 12:48:55 +0100

    Initial Commit

---
+++ primeCheck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  primeCheck
+//
+//  Created by Paulo Custódio on 30/07/2020.
+//  Copyright © 2020 Paulo Custódio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
